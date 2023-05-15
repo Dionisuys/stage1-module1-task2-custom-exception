88cf6d9ff75036656fef826d51e581b7684fa230
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-05-16 00:15:45 +0300

    Task 2 solution

---
+++ src/main/java/com/epam/mjc/StudentManagerExceptions.java
@@ -0,0 +1,7 @@
+package com.epam.mjc;
+
+public class StudentManagerExceptions extends IllegalArgumentException {
+    public StudentManagerExceptions(String message) {
+        super(message);
+    }
+}
